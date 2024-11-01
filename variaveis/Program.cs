e4db6ad5a3892eb2cc9454918e6806450af332cf
Paulo André <pauloandrearaujofernandes@gmail.com>
2024-11-01 16:31:46 -0400

    Update Program.cs

--- variaveis/Program.cs
+++ variaveis/Program.cs
@@ -18,4 +18,41 @@ char inicial = "W";
 bool estaLogado = true;
 bool ativo = false;
 
+// Tipos de dados Nao primitivos 
+string saudacao = "Olá mundo!";
 
+//Arrays
+int[] numeros = {1, 2, 3, 4, 5};
+string[] nomes = {"Paulo Andre", "Joel Fernandes", "Lidia Fernandes"};
+
+//Classes
+public class Pessoa{
+
+    public string Nome;
+    public int idade;
+}
+
+//Structs 
+public class Ponto
+{
+    public int X;
+    public int Y;
+}
+
+//Enums 
+public enum diaDaSemana
+{
+    Segunda, Terca, Quarta, Quinta, Sexta, Sabado, Domingo
+}
+
+//Interfaces
+public interface IVeiculo
+{
+    void Mover();
+}
+
+// Delegates 
+public void Delegates Processar(int numero);
+
+//Nullable Types
+int? idade = null;
