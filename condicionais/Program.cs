9a125fb9e04cb4d247c89ad0e3399bf5bd677929
Paulo André <pauloandrearaujofernandes@gmail.com>
2024-11-08 17:22:53 -0400

    a

---
+++ condicionais/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Switch Case
+Console.WriteLine("Sabado\n");
+string dia = Console.WriteLine();
+
+if (dia == "Sabado"|| dia == "Domingo" || dia =="Segunda" || dia == "Terca" || dia == "Quarta" || dia == "Quinta" || dia == "Sexta"){
+    switch (dia){}
+
+ 
+ bool 
