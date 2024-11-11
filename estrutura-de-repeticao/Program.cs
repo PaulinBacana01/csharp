1b32384fec58943165bf332ed7b64670fbfa2002
Paulo André <pauloandrearaujofernandes@gmail.com>
2024-11-11 17:29:39 -0400

    estrutura de repeticao kkk

---
+++ estrutura-de-repeticao/Program.cs
@@ -0,0 +1,35 @@
+﻿//Estrutura de Repeticao while
+//Random numeroAleatorio = new Random();
+//int numero = numeroAleatorio.Next(1,1000);
+//int contador = 0;
+//Console.WriteLine($"Numero Alvo: {numero}");
+//Loop que incrementa o contador ate ele atingir o numero alvo
+//while (contador != numero)
+//{
+   // Console.WriteLine($"Contador: {contador}");
+    //contador++;
+//}
+//Console.WriteLine($"Contador atingiu o numero alvo:{contador}");
+
+//Estrutura de Repeticao do-While
+//int numero = 0;
+
+//do
+//{
+   // Console.WriteLine($"Numero: {numero}");
+    //numero++;
+
+//} while (numero < 100);
+
+//for (int i = 0; i <=10; i++ )
+//{
+   // Console.WriteLine($"Valor de: i: {i}");
+//}
+
+string[] nomes = {"Paulo", "Joao", "Lidia"};
+foreach (string nome in nomes)
+{
+    Console.WriteLine(nome);
+}
+
+
